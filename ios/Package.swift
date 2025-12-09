16d674e55e54d01563891496bab69ff1f5a81cd0
zolkko <zolkko@gmail.com>
2025-12-09 14:37:42 +0100

    Update deployment target to iOS 18 in Package.swift

--- ios/Package.swift
+++ ios/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SkjuIOS",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v18)
     ],
     products: [
         .library(name: "SkjuIOS", targets: ["SkjuIOS"]),
