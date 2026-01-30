ffdad5371b02f5355fab39f256b696a96b9ca06e
zolkko <zolkko@gmail.com>
2026-01-30 17:02:12 +0100

    Refactor error handling in `defmt_cs` by introducing `DefmtError` and using `anyhow`
    - Replaced `DefmtCsError` with `DefmtError` for clearer and more extensible error handling.
    - Leveraged `anyhow` for enhanced error propagation.
    - Updated `Result` type and refactored error conversions in `DefmtDecoder`.

--- defmt_cs/src/lib.rs
+++ defmt_cs/src/lib.rs
@@ -1,21 +1,32 @@
 use defmt_decoder::Table;
 use std::fs;
 use std::sync::{Arc, Mutex};
+use uniffi::deps::anyhow;
 
+#[derive(Debug, thiserror::Error, uniffi::Object)]
+#[error(transparent)]
+#[uniffi::export(Debug, Display)]
+pub struct DefmtError {
+    #[from]
+    error: DefmtInnerError,
+}
 
-#[derive(Debug, thiserror::Error, uniffi::Error)]
-pub enum DefmtCsError {
+#[derive(Debug, thiserror::Error)]
+enum DefmtInnerError {
     #[error(".defmt data not found")]
     DefmtNotFound,
 
-    #[error("IO error")]
-    Io,
+    #[error(transparent)]
+    DecodeError(#[from] defmt_decoder::DecodeError),
 
-    #[error("Anyhow")]
-    Anyhow,
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Anyhow(#[from] anyhow::Error),
 }
 
-type Result<T> = std::result::Result<T, DefmtCsError>;
+type Result<T> = std::result::Result<T, DefmtError>;
 
 #[derive(uniffi::Object)]
 struct DefmtDecoder {
@@ -26,15 +37,21 @@ struct DefmtDecoder {
 impl DefmtDecoder {
     #[uniffi::constructor]
     fn new(elf_path: String) -> Result<Self> {
-        let bytes = fs::read(&elf_path).map_err(|_| DefmtCsError::Io)?;
-        let table = Table::parse(&bytes).map_err(|_| DefmtCsError::Anyhow)?.ok_or_else(|| DefmtCsError::DefmtNotFound)?;
+        let bytes = fs::read(&elf_path).map_err(DefmtInnerError::from)?;
+        let table = Table::parse(&bytes)
+            .map_err(DefmtInnerError::from)?
+            .ok_or_else(|| DefmtInnerError::DefmtNotFound)?;
         Ok(Self { table: Arc::new(Mutex::new(table)) })
     }
 
     #[uniffi::method(name = "DecodeFrame")]
     fn decode_frame(&self, data: &[u8]) -> Result<String> {
-        let table_lock = self.table.lock().expect("failed to aquire a lock for the table object");
-        let (frame, _idx) = table_lock.decode(data).map_err(|_| DefmtCsError::Anyhow)?;
+        let table_lock = self
+            .table
+            .lock()
+            .expect("failed to acquire a lock for the table object");
+        let (frame, _idx) = table_lock
+            .decode(data).map_err(DefmtInnerError::from)?;
         Ok(frame.display(false).to_string())
     }
 }
