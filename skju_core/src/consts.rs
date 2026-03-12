6f2e749b09793ffbf3042d105aa08948dfab5b72
zolkko <zolkko@gmail.com>
2026-03-12 07:46:46 +0100

    Add physical constants to the core crate

---
+++ skju_core/src/consts.rs
@@ -0,0 +1,5 @@
+/// P-Wave speed km/S
+pub const P_WAVE: f64 = 6.0;
+
+/// S-Wave speed km/S
+pub const S_WAVE: f64 = 3.5;
--- skju_core/src/lib.rs
+++ skju_core/src/lib.rs
@@ -2,5 +2,9 @@ mod common;
 pub mod filter;
 pub mod sensor;
 pub mod utils;
+mod consts;
+
 
 pub use common::*;
+
+pub use consts::*;
